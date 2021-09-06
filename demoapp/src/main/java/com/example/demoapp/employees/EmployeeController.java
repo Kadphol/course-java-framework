fb3217fd9b440e41d978cc329244302c999dc555
Kadphol <kadphol.len@outlook.co.th>
2021-09-06 16:08:49 +0700

    Add get employee by id

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
@@ -1,19 +1,30 @@
 package com.example.demoapp.employees;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    @GetMapping("/employees/{id}")
+    public EmployeeResponse getEmployeeById(@PathVariable String id) {
+//       Validate input
+//       Claening data
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(Integer.parseInt(id));
+        employee01.setName("somkiat");
+        return employee01;
+    }
+
     @GetMapping("/employees")
-    public EmployeeResponse[] getAllEmployees() {
+    public EmployeeResponse[] listEmployee() {
         EmployeeResponse employee01 = new EmployeeResponse();
         employee01.setId(1);
-        employee01.setName("mark");
+        employee01.setName("somkiat");
         EmployeeResponse employee02 = new EmployeeResponse();
         employee02.setId(2);
-        employee02.setName("mark 2");
+        employee02.setName("somkiat 2");
         return new EmployeeResponse[]{employee01, employee02};
     }
 }
