0b5d592781fc678b7281e19d93434b5b5a642dc4
Kadphol <kadphol.len@outlook.co.th>
2021-09-06 14:53:41 +0700

    Add employee for getAllEmployees

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
@@ -8,6 +8,12 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public EmployeeResponse[] getAllEmployees() {
-        return new EmployeeResponse[]{new EmployeeResponse()};
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(1);
+        employee01.setName("mark");
+        EmployeeResponse employee02 = new EmployeeResponse();
+        employee02.setId(2);
+        employee02.setName("mark 2");
+        return new EmployeeResponse[]{employee01, employee02};
     }
 }
