6ea1e1255c3022f53c35645c6c44c6d2c0cec1b2
Kadphol <kadphol.len@outlook.co.th>
2021-09-07 11:53:23 +0700

    Failure Test

---
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerFailureTest.java
@@ -0,0 +1,32 @@
+package com.example.demoapp.employees;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerFailureTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("error 404, employee id = 1 not found")
+    public void case01() {
+        //Arrange
+        int id = 1;
+        //Act
+        ErrorResponse result
+                = restTemplate.getForObject("/employees/" +id, ErrorResponse.class);
+        //Assert
+        assertEquals(404, result.getCode());
+        assertEquals("Employee not found id=1",result.getDetail());
+    }
+}
