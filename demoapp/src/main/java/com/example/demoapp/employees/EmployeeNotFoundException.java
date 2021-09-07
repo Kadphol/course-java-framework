42d2e38f4cb1c6876f0c50e2feb27638f802ceb2
Kadphol <kadphol.len@outlook.co.th>
2021-09-07 10:40:27 +0700

    Add Custom Exception

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeNotFoundException.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.demoapp.employees;public class EmployeeNotFoundException {
+package com.example.demoapp.employees;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
 }
