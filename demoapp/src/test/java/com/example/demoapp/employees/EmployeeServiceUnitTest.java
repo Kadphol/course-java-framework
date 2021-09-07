c3095a335ba7406dd8a4b2f911738e5e889be053
Kadphol <kadphol.len@outlook.co.th>
2021-09-07 14:41:26 +0700

    Fail case: Solution 1

--- demoapp/src/test/java/com/example/demoapp/employees/EmployeeServiceUnitTest.java
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeServiceUnitTest.java
@@ -1,19 +1,54 @@
 package com.example.demoapp.employees;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceUnitTest {
 
+    @Mock
+    private EmployeeRepository employeeRepository;
+
     @Test
-    public void getById() {
+    @DisplayName("Success case")
+    public void case01() {
         //Arrange
-        EmployeeService employeeService = new EmployeeService();
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Mock");
+        when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
         //Act
         EmployeeResponse result = employeeService.getById(1);
         //Assert
         assertEquals(1, result.getId());
-        assertEquals("Mock name", result.getName());
+        assertEquals("Mock", result.getName());
     }
+
+    @Test
+    @DisplayName("Fail case : Employee not found")
+    public void case02() {
+        String message = null;
+        when(employeeRepository.findById(100)).thenReturn(Optional.empty());
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+
+        try {
+            EmployeeResponse result = employeeService.getById(100);
+            fail();
+        } catch(EmployeeNotFoundException e) {
+            if(!"Employee not found id=100".equals(e.getMessage())) {
+                fail("Message fail with = " + e.getMessage());
+            }
+        }
+    }
+
 }
